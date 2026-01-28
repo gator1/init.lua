f0ce96cce2b6bf52846d4fa1c30aebef748c5ff4
Gary Xia <ilovegators@gmail.com>
2026-01-28 10:15:08 -0800

    remove some gitsigns keymaps

--- lua/theprimeagen/lazy/gitsigns.lua
+++ lua/theprimeagen/lazy/gitsigns.lua
@@ -42,16 +42,8 @@ return {
             local gs = require("gitsigns")
 
             -- Keymaps
-            vim.keymap.set("n", "<leader>gbl", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
             vim.keymap.set("n", "<leader>gb", gs.blame_line, { desc = "Show blame" })
-            vim.keymap.set("n", "<leader>gd", gs.diffthis, { desc = "Diff this" })
-            vim.keymap.set("n", "<leader>gD", function() gs.diffthis("~") end, { desc = "Diff against parent" })
-            vim.keymap.set("n", "<leader>ghs", gs.stage_hunk, { desc = "Stage hunk" })
-            vim.keymap.set("n", "<leader>ghr", gs.reset_hunk, { desc = "Reset hunk" })
-            vim.keymap.set("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage buffer" })
-            vim.keymap.set("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset buffer" })
-            vim.keymap.set("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
-            vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+            vim.keymap.set("n", "<leader>gbl", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
         end,
     }
 }
